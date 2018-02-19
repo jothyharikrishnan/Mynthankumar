8b3348bf0621a1683928d9ca1b3d378024c63ed2
Mynthankumar <36624741+Mynthankumar@users.noreply.github.com>
2018-02-19 17:36:16 +0530

    Create SET1-1.cpp

---
+++ SET1-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a;
+cout<<"enter the value\n";
+cin>>a;
+if(a>0)
+{
+	cout<<"the number is positive";
+}
+else if(a<0)
+{
+	cout<<"the number is negative";
+}
+else
+{
+	cout<<"Zero";
+}
+	return 0;
+}
