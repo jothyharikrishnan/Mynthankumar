3675102d65b7f7e7dac62a8450afabaa870e4184
Mynthankumar <36624741+Mynthankumar@users.noreply.github.com>
2018-02-20 16:54:52 +0530

    Create SET1-3.cpp

---
+++ SET1-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+      char mm;
+      cout<<"enter the character\n";
+      cin>>mm;
+      if(mm=='a'||mm=='e'||mm=='i'||mm=='o'||mm=='u')
+      {
+      	cout<<"the given character is vowels";
+      }
+      else
+      {
+      		cout<<"the given character  is constent";
+      }
+	return 0;
+}
